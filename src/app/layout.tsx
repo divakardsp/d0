ce2a8cdd60f1009af9053dd56ea9ce3306b79982
divakar singh purva <divakarpurva04@gmail.com>
2026-07-13 23:55:51 +0530

    feat: integrate @tanstack/react-query and add QueryProvider for data fetching

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Outfit, Raleway } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const ralewayHeading = Raleway({
     subsets: ["latin"],
@@ -51,7 +52,7 @@ export default function RootLayout({
                     enableSystem
                     disableTransitionOnChange
                 >
-                    {children}
+                    <QueryProvider>{children}</QueryProvider>
                 </ThemeProvider>
             </body>
         </html>
